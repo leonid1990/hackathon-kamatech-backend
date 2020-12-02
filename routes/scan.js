03d81a98b6b99fc428c4301aee99128ca2d4a0a0
ELK4N4 <elkana.hendler@gmail.com>
2020-12-03 01:03:28 +0200

    Added scan/:roomid

--- routes/scan.js
+++ routes/scan.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET*/
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* GET*/
+router.get('/:roomid', function(req, res, next) {
+    const roomid = req.params.roomid;
+    res.render('index', { title: 'roomid' });
+  });
 
 module.exports = router;
