6c37713d2765e34c3e43cb5a73e5e75b4fe82c12
ELK4N4 <elkana.hendler@gmail.com>
2020-12-03 11:09:31 +0200

    enable cors

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if(process.env.NODE_ENV !== 'production') {
 
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -22,6 +23,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
